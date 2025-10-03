1c21ebd9f5df3526ad51876ca005b803d96c2834
urrahmandhiya <urrahmand0@gmail.com>
2025-10-03 23:45:59 +0700

    07_repeatString done

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function(str, amount) {
+    let result = "";
+    if (amount < 0) return "ERROR";
+    for (let i = 0; i < amount; i++) result += String(str);
+    return result; 
 };
 
 // Do not edit below this line
