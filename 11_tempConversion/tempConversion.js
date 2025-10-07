f77cd325a836fa3c32c0282700ee85b45697e1f9
urrahmandhiya <urrahmand0@gmail.com>
2025-10-08 02:07:42 +0700

    11_tempConversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,10 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let celcius = (temp - 32) * 5/9;
+  return Math.round(celcius * 10) / 10;
 };
-
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let farenheit = (temp * 9) / 5 + 32;
+  return Math.round(farenheit * 10) / 10;
 };
 
 // Do not edit below this line
