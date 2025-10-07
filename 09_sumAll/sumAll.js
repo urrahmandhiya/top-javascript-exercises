448db1b22cf91738bfd47ac41d3403b50b5e0456
urrahmandhiya <urrahmand0@gmail.com>
2025-10-08 01:43:20 +0700

    09_sumAll done

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (
+        !Number.isInteger(num1) || 
+        !Number.isInteger(num2) ||
+        num1 < 0 ||
+        num2 < 0
+    ) return 'ERROR'
 
+    let first = Math.min(num1, num2);
+    let n = Math.max(num1, num2);
+    return ((n - first + 1)/2 * (first + n))
 };
 
 // Do not edit below this line
