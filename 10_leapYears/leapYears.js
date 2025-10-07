12403e5e173b12aaaa65de1b8923139e63b1d938
urrahmandhiya <urrahmand0@gmail.com>
2025-10-08 01:49:51 +0700

    10_leapYears done

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let leap = false;
+    if (year % 4 === 0) leap = true;
+    if (year % 100 === 0) leap = false;
+    if (year % 400 === 0) leap = true;
+    return leap
 };
 
 // Do not edit below this line
