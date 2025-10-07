e45bde35af58f5f28a2e5ac0b0a6a503296fd67f
urrahmandhiya <urrahmand0@gmail.com>
2025-10-08 01:15:25 +0700

    08_removeFromArray done

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...items) {
+    return arr.reduce((acc, curr) => {
+        if (!items.includes(curr)) acc.push(curr);
+        return acc;
+    }, [])
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
