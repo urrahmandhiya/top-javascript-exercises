333e4f44346186c0b034b37daee9607b3721e518
urrahmandhiya <urrahmand0@gmail.com>
2026-02-14 16:16:27 +0700

    13_palindromes done

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let cleanedString = str
+        .replace(/[ .,\/#!$%\^&\*;:{}=\-_`~()]/g, "")
+        .toLowerCase();
+    
+    let left = 0;
+    let right = cleanedString.length - 1;
 
+    while (left < right) {
+        if (cleanedString[left] !== cleanedString[right]) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return true;
 };
 
 // Do not edit below this line
