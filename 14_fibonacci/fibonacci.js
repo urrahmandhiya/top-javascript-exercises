c3cd6170b833e76aa66868b3b12b92ff77ee12db
urrahmandhiya <urrahmand0@gmail.com>
2026-02-14 16:46:35 +0700

    14_fibonacci done

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+    let num = Number(input);
+    if (num === 0) return 0;
+    if (num < 0) return "OOPS";
 
+    let seq = [1, 1];
+    while (seq.length <= num) {
+        let cur = seq[seq.length - 1] + seq[seq.length - 2];
+        seq.push(cur)
+    }
+    return seq[num-1];
 };
 
 // Do not edit below this line
