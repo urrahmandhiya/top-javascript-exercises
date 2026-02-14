a8a3f7ad61128e8b4f42f9f487c03884ede02197
urrahmandhiya <urrahmand0@gmail.com>
2026-02-14 17:58:24 +0700

    16_findTheOldest done

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,28 @@
-const findTheOldest = function() {
-
+const getAge = (birth, death) => {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
+const findTheOldest = function (people) {
+    return people.reduce((old, cur) => {
+        const oldestAge = getAge(old.yearOfBirth, old.yearOfDeath);
+        const currentAge = getAge(cur.yearOfBirth, cur.yearOfDeath);
+        return oldestAge < currentAge ? cur : old;
+    })
 };
+// My old answer:
+// const findTheOldest = function (people) {
+//   return people
+//     .map((person) => {
+//       if (!Object.hasOwn(person, "yearOfDeath")) {
+//         person.yearOfDeath = new Date().getFullYear();
+//       }
+//       person["age"] = person.yearOfDeath - person.yearOfBirth;
+//       return person;
+//     })
+//     .sort((a, b) => b.age - a.age)[0];
+// };
 
 // Do not edit below this line
 module.exports = findTheOldest;
