32354ec82210b58901764f2acf2f001cb73c9479
urrahmandhiya <urrahmand0@gmail.com>
2026-02-14 16:03:07 +0700

    12_calculator done

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((acc, cur) => acc + cur, 0);
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  return arr.reduce((acc, cur) => acc * cur, 1);
 
 };
 
-const power = function() {
-	
+const power = function(base, expo) {
+	return base ** expo;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 1 || num === 0) return 1;
+  let sum = num;
+  while (num > 1) {
+    num--;
+    sum *= num;
+  }
+  return sum;
 };
 
 // Do not edit below this line
